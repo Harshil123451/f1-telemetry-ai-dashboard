8fab8d260e4fd8285e23be0eae1bd8448db90c50
Parzefal-ai <kchh0007@student.monash.edu>
2026-05-02 18:32:25 +1000

    fix: enable CORS for deployment

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all for now
+    allow_origins=["*"],  # for now (later we restrict)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
